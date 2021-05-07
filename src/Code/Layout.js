fe26b5a3b5c32fe4678fed8053bbd6a698b89993
Cecilia Cruz <cruzscecilia@gmail.com>
2021-05-07 12:40:04 -0700

    removed background onclick event

--- src/Code/Layout.js
+++ src/Code/Layout.js
@@ -3,19 +3,8 @@ import Nav from './Menu.js'
 import Posts from './Posts.js';
 
 function Layout () {
-    const closeModal = () => {
-        const modal = document.querySelectorAll(".modal");
-          if (modal.length > 0) {
-              for (let i = 0; i < modal.length; i++) {
-                modal[i].classList.remove("active");
-              }
-            }
-          else {
-            console.log("already closed");
-          } 
-      }
     return (
-        <div className="outerContainer" onClick={() => closeModal()}>
+        <div className="outerContainer">
             <div className="header">
                 <div className="webName">MyWebsite</div>
                 <Nav />
